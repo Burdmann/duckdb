edde36bbb101fa003f50e4fe5167513e62e1e165
altp <altp@itu.dk>
2025-12-01 17:52:20 +0100

    .

--- run_tpch.sh
+++ run_tpch.sh
@@ -1,13 +1,13 @@
-# dir="xperiments_results/array"
-dir="."
-# mkdir $dir
-# mkdir $dir/processed
+dir="xperiments_results/hashmap"
+# dir="."
+mkdir $dir
+mkdir $dir/processed
 cat template.csv > $dir/raw_data_min_max.csv
 cat template.csv > $dir/raw_data_cluster.csv
 cat template.csv > $dir/raw_data_bloom.csv
-./build/release/duckdb_min_max_hashmap -f sql_commands_tpch_1.txt 2>> $dir/raw_data_min_max.csv > output_min_max.txt
-./build/release/duckdb_cluster_hashmap -f sql_commands_tpch_1.txt 2>> $dir/raw_data_cluster.csv > output_cluster.txt
-./build/release/duckdb_bloom_hashmap -f sql_commands_tpch_1.txt 2>> $dir/raw_data_bloom.csv > output_bloom.txt
-# python3 data_processor_tpch.py $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
-# mv pruning_tpch.csv $dir/processed/
-# mv times_tpch.csv $dir/processed/
+./build/release/duckdb_min_max_hashmap -f sql_commands_tpch.txt 2>> $dir/raw_data_min_max.csv
+./build/release/duckdb_cluster_hashmap -f sql_commands_tpch.txt 2>> $dir/raw_data_cluster.csv
+./build/release/duckdb_bloom_hashmap -f sql_commands_tpch.txt 2>> $dir/raw_data_bloom.csv
+python3 data_processor_tpch.py $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
+mv pruning_tpch.csv $dir/processed/
+mv times_tpch.csv $dir/processed/
