bcf675c2ec0a56a360681806087ddae9697cf69b
altp <altp@itu.dk>
2025-12-11 22:32:21 +0100

    .

--- run_tpch.sh
+++ run_tpch.sh
@@ -6,7 +6,7 @@ dir="xperiments_results/before_between"
 # cat template.csv > $dir/raw_data_cluster.csv
 # cat template.csv > $dir/raw_data_bloom.csv
 # ./build/release/duckdb -f sql_commands_tpch.txt 2>> $dir/raw_data_min_max.csv > output3.txt
-./build/release/duckdb -f sql_commands_tpch.txt 2> /dev/null > output4.txt
+./build/release/duckdb -f sql_commands_tpch.txt 2> /dev/null > output5.txt
 # ./build/release/duckdb_cluster -f sql_commands_tpch.txt 2>> $dir/raw_data_cluster.csv
 # ./build/release/duckdb_bloom -f sql_commands_tpch.txt 2>> $dir/raw_data_bloom.csv
 # python3 data_processor_tpch.py $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
