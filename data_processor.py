cea658808b40e80d11f52fc06b364d41b59f740f
altp <altp@itu.dk>
2025-12-15 17:56:16 +0100

    made changes to running and data processing

--- data_processor.py
+++ data_processor.py
@@ -1,38 +1,50 @@
 import duckdb
 import sys
 
-in_file = sys.argv[1]
-out_file = sys.argv[2]
+NUM_TABLES = int(sys.argv[1])
+NUM_QUERIES = int(sys.argv[2])
+NUM_ITERATIONS = int(sys.argv[3])
+in_files = sys.argv[4:]
 
-duckdb.execute(f"CREATE TABLE tbl AS SELECT * FROM '{in_file}';")
+NUM_STATS = len(in_files)
+QUERIES_START = NUM_TABLES+NUM_QUERIES+2
 
-names = duckdb.execute("SELECT DISTINCT CAST(Data->'type' AS VARCHAR) FROM tbl WHERE type = 'END_INITIALISE_ADDITIONAL_STATS';").fetchall()
-names = f"\"{";".join([tpl[0].replace('"','') for tpl in names])}\""
+times = [[] for _ in range(NUM_STATS)]
+rows_scanned = [[] for _ in range(NUM_STATS)]
+sizes = [[] for _ in range(NUM_STATS)]
+ingestion_times = [[] for _ in range(NUM_STATS)]
+tables = [[] for _ in range(NUM_STATS)]
 
-size = duckdb.execute("SELECT SUM(CAST(Data->'size' AS INTEGER)) FROM tbl WHERE type = 'END_INITIALISE_ADDITIONAL_STATS';").fetchone()[0]
-# rowgroups_scanned = duckdb.execute("SELECT COUNT(DISTINCT CAST(Data->'partition' AS VARCHAR)) FROM tbl WHERE type = 'SCANNED_ROWS';").fetchone()[0]
-rows_scanned = duckdb.execute("SELECT SUM(CAST(Data->'count' AS INTEGER)) FROM tbl WHERE type = 'SCANNED_ROWS';").fetchone()[0]
-additional_stat_processing_time = duckdb.execute("SELECT ROUND(SUM(time-CAST(Data->'start_time' AS BIGINT))/1000000000,3) FROM tbl WHERE type = 'EVAL_ADDITIONAL_STATISTICS_END';").fetchone()[0]
-stat_processing_time = duckdb.execute("SELECT ROUND(SUM(time-CAST(Data->'start_time' AS BIGINT))/1000000000,3) FROM tbl WHERE type = 'EVAL_STATISTICS_END';").fetchone()[0]
-production_time = duckdb.execute("SELECT ROUND(SUM(time-CAST(Data->'start_time' AS BIGINT))/1000000000,3) FROM tbl WHERE type = 'END_INITIALISE_ADDITIONAL_STATS';").fetchone()[0]
-ingestion_time = duckdb.execute("SELECT ROUND(SUM(time-CAST(Data->'start_time' AS BIGINT))/1000000000,3) FROM tbl WHERE type = 'APPEND_END';").fetchone()[0]
+duckdb.execute("CREATE TABLE Result_time (Query INT, min_max_time FLOAT, cluster_time FLOAT, bloom_time FLOAT);")
+duckdb.execute("CREATE TABLE Result_pruning (Query INT, min_max_rows_scanned FLOAT, cluster_rows_scanned FLOAT, bloom_rows_scanned FLOAT);")
+duckdb.execute("CREATE TABLE Result_size (Table_name VARCHAR, min_max_rows_scanned FLOAT, cluster_rows_scanned FLOAT, bloom_rows_scanned FLOAT);")
+duckdb.execute("CREATE TABLE Result_ingestion (Table_name VARCHAR, min_max_rows_scanned FLOAT, cluster_rows_scanned FLOAT, bloom_rows_scanned FLOAT);")
+for idx,in_file in enumerate(in_files):
+    duckdb.execute(f"CREATE TABLE tbl AS SELECT * FROM '{in_file}';")
+    duckdb.execute("CREATE TABLE times AS SELECT start_time,end_time,t1.CommandID AS CommandID FROM (SELECT Time as end_time,CommandID FROM tbl WHERE type = 'SQL_COMMAND_RUN_END') t1 JOIN (SELECT Time as start_time,CommandID FROM tbl WHERE type = 'SQL_COMMAND_RUN_START') t2 on t1.CommandID = t2.commandID;")
 
-duckdb.execute("CREATE TABLE command_times AS SELECT CommandID AS id,MAX(time)-MIN(time) AS time_diff FROM tbl WHERE type = 'SQL_COMMAND_RUN_START' or type = 'SQL_COMMAND_RUN_END' GROUP BY CommandID;")
-total_build_time = duckdb.execute("SELECT ROUND(time_diff/1000000000,3) FROM command_times WHERE id = 0;").fetchone()[0]
-avg_total_query_time = duckdb.execute("SELECT ROUND(AVG(time_diff)/1000000000,3) FROM command_times WHERE id != 0;").fetchone()[0]
+    for i in range(NUM_TABLES):
+        sizes[idx].append(duckdb.execute(f"SELECT COALESCE(SUM(CAST(Data->'size' AS INTEGER)),0) FROM tbl WHERE type = 'END_INITIALISE_ADDITIONAL_STATS' AND CommandID = {i};").fetchone()[0])
+        ingestion_times[idx].append(duckdb.execute(f"SELECT ROUND((end_time-start_time)/1000000000.0,3) FROM times WHERE CommandID = {i}").fetchone()[0])
+        tables[idx].append(duckdb.execute(f"SELECT CAST(Data->'command' AS VARCHAR) FROM tbl WHERE type='SQL_COMMAND_RUN_START' AND CommandID = {i}").fetchone()[0].split()[2])
 
+    for i in range(NUM_QUERIES):
+        times[idx].append(duckdb.execute(f"SELECT ROUND(MEDIAN(end_time-start_time)/1000000000.0,3) FROM times WHERE CommandID >= {QUERIES_START+NUM_ITERATIONS*i} AND CommandID < {QUERIES_START+NUM_ITERATIONS*(1+i)};").fetchone()[0])
+        rows_scanned[idx].append(duckdb.execute(f"SELECT ROUND(SUM(CAST(Data->'count' AS INTEGER))/1000000.0,2) AS val FROM tbl WHERE type = 'SCANNED_ROWS' and CommandID = {NUM_TABLES+1+i};").fetchone()[0])
 
+    duckdb.execute(f"DROP TABLE tbl;")
+    duckdb.execute(f"DROP TABLE times;")
+print(sizes)
+for i in range(NUM_QUERIES):
+    duckdb.execute(f"INSERT INTO Result_time VALUES ({i+1},{",".join([str(time[i]) for time in times])});")
+    duckdb.execute(f"INSERT INTO Result_pruning VALUES ({i+1},{",".join([str(scanned[i]) for scanned in rows_scanned])});")
+for i in range(NUM_TABLES):
+    if (len(set([tables[j][i] for j in range(NUM_STATS)]))) != 1:
+        raise "tables are not in the same order in given input files"
+    duckdb.execute(f"INSERT INTO Result_size VALUES ('{tables[0][i]}',{",".join([str(size[i]) for size in sizes])});")
+    duckdb.execute(f"INSERT INTO Result_ingestion VALUES ('{tables[0][i]}',{",".join([str(time[i]) for time in ingestion_times])});")
 
-empty = True
-try:
-    with open(out_file,'r') as f:
-        content = f.read()
-        if len(content) > 0:
-            empty = False
-except:
-    pass
-
-with open(out_file,'a+') as f:
-    if empty:
-        f.write("name,size,rows_scanned,additional_stat_processing_time,stat_processing_time,production_time,ingestion_time,total_build_time,avg_query_time\n")
-    f.write(f"{names},{size},{rows_scanned},{additional_stat_processing_time},{stat_processing_time},{production_time},{ingestion_time},{total_build_time},{avg_total_query_time}\n")
+duckdb.execute("COPY Result_time TO 'query_time.csv';")
+duckdb.execute("COPY Result_pruning TO 'pruning.csv';")
+duckdb.execute("COPY Result_size TO 'size.csv';")
+duckdb.execute("COPY Result_ingestion TO 'ingestion.csv';")
--- run_tpch.sh
+++ run_tpch.sh
@@ -1,16 +1,14 @@
-dir="xperiments_results/before_between"
+dir="xperiments_results/tpch"
 # dir="."
-mkdir $dir
-mkdir $dir/processed
-cat template.csv > $dir/raw_data_min_max.csv
-cat template.csv > $dir/raw_data_cluster.csv
-cat template.csv > $dir/raw_data_bloom.csv
-./build/release/duckdb -f sql_commands_tpch.txt 2>> $dir/raw_data_min_max.csv
-./build/release/duckdb -f sql_commands_tpch_analyse.txt 2>> $dir/raw_data_min_max.csv
-./build/release/duckdb_cluster -f sql_commands_tpch.txt 2>> $dir/raw_data_cluster.csv
-./build/release/duckdb_bloom -f sql_commands_tpch.txt 2>> $dir/raw_data_bloom.csv
-python3 data_processor_tpch.py $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
-mv pruning_tpch.csv $dir/processed/
-mv times_tpch.csv $dir/processed/
-mv compile_times_tpch.csv $dir/processed/
-mv running_times_tpch.csv $dir/processed/
+# mkdir -p $dir/processed
+# cat template.csv > $dir/raw_data_min_max.csv
+# cat template.csv > $dir/raw_data_cluster.csv
+# cat template.csv > $dir/raw_data_bloom.csv
+# ./build/release/duckdb_min_max -f sql_queries_tpch.txt 2>> $dir/raw_data_min_max.csv
+# ./build/release/duckdb_cluster -f sql_queries_tpch.txt 2>> $dir/raw_data_cluster.csv
+# ./build/release/duckdb_bloom -f sql_queries_tpch.txt 2>> $dir/raw_data_bloom.csv
+python3 data_processor.py 8 22 80 $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
+mv pruning.csv $dir/processed/
+mv query_time.csv $dir/processed/
+mv size.csv $dir/processed/
+mv ingestion.csv $dir/processed/
