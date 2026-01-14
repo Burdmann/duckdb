16a1c804a6fea677f5ed9a0c47caffaf332235b0
Alexander Pedersen <altp@itu.dk>
2026-01-14 11:43:40 +0100

    updated test queries

--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,7 @@ cat template.csv > $dir/raw_data_bloom.csv
 ./build/release/duckdb_min_max -f sql_queries_test_shipdate.txt 2>> $dir/raw_data_min_max.csv
 ./build/release/duckdb_cluster -f sql_queries_test_shipdate.txt 2>> $dir/raw_data_cluster.csv
 ./build/release/duckdb_bloom -f sql_queries_test_shipdate.txt 2>> $dir/raw_data_bloom.csv
-python3 data_processor.py 1 200 1 $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
+python3 data_processor.py 1 200 50 $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
 mv pruning.csv $dir/processed/
 mv query_time.csv $dir/processed/
 mv size.csv $dir/processed/
