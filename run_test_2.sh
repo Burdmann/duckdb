d10268ca2efa5d5ae0082b0c64761b74a8d60682
altp <altp@itu.dk>
2026-01-29 00:06:11 +0100

    ran experiments on l_comment

---
+++ run_test_2.sh
@@ -0,0 +1,14 @@
+dir="xperiments_results/test_comment"
+# dir="."
+mkdir -p $dir/processed
+cat template.csv > $dir/raw_data_min_max.csv
+cat template.csv > $dir/raw_data_cluster.csv
+cat template.csv > $dir/raw_data_bloom.csv
+./build/release/duckdb_min_max -f sql_queries_test_2.sql 2>> $dir/raw_data_min_max.csv
+./build/release/duckdb_cluster -f sql_queries_test_2.sql 2>> $dir/raw_data_cluster.csv
+./build/release/duckdb_bloom -f sql_queries_test_2.sql 2>> $dir/raw_data_bloom.csv
+python3 data_processor.py 1 100 50 $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv
+mv pruning.csv $dir/processed/
+mv query_time.csv $dir/processed/
+mv size.csv $dir/processed/
+mv ingestion.csv $dir/processed/
