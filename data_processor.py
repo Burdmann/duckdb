afaf3a5f58fbcff828328c7ca22e24fba132bb5f
Alexander Pedersen <altp@itu.dk>
2026-02-06 23:13:30 +0100

    worked on visualiser

--- data_processor.py
+++ data_processor.py
@@ -7,7 +7,7 @@ NUM_QUERIES = int(sys.argv[2])
 NUM_ITERATIONS = int(sys.argv[3])
 in_files = sys.argv[4:]
 
-ATTACHED = True
+ATTACHED = False # FIXME:
 NUM_STATS = len(in_files)
 QUERIES_START = 2*NUM_TABLES+NUM_QUERIES+2 + (2 if ATTACHED else 0)
 # names = ["_".join(infile.split("/")[-1].split('.')[0].split('_')[2:]) for infile in in_files]
@@ -23,10 +23,10 @@ if os.path.exists("test.db"):
     os.remove("test.db")
 duckdb.execute("ATTACH 'test.db'")
 duckdb.execute("USE test")
-duckdb.execute(f"CREATE TABLE Result_time (Query INT, {", ".join([f'{name} FLOAT' for name in names])});")
-duckdb.execute(f"CREATE TABLE Result_pruning (Query INT, {", ".join([f'{name} FLOAT' for name in names])});")
-duckdb.execute(f"CREATE TABLE Result_size (Table_name VARCHAR, {", ".join([f'{name} INT64' for name in names])});")
-duckdb.execute(f"CREATE TABLE Result_ingestion (Table_name VARCHAR, {", ".join([f'{name} FLOAT' for name in names])});")
+duckdb.execute(f"CREATE TABLE Result_time (Name VARCHAR, Unit VARCHAR, Query INT, {", ".join([f'"{name}" FLOAT' for name in names])});")
+duckdb.execute(f"CREATE TABLE Result_pruning (Name VARCHAR, Unit VARCHAR, Query INT, {", ".join([f'"{name}" FLOAT' for name in names])});")
+duckdb.execute(f"CREATE TABLE Result_size (Name VARCHAR, Unit VARCHAR, Table_name VARCHAR, {", ".join([f'"{name}" INT64' for name in names])});")
+duckdb.execute(f"CREATE TABLE Result_ingestion (Name VARCHAR, Unit VARCHAR, Table_name VARCHAR, {", ".join([f'"{name}" FLOAT' for name in names])});")
 for idx,in_file in enumerate(in_files):
     duckdb.execute(f"CREATE TABLE tbl AS SELECT * FROM read_csv('{in_file}',max_line_size=100000000);")
     duckdb.execute("CREATE TABLE times AS SELECT start_time,end_time,t1.CommandID AS CommandID FROM (SELECT Time as end_time,CommandID FROM tbl WHERE type = 'SQL_COMMAND_RUN_END') t1 JOIN (SELECT Time as start_time,CommandID FROM tbl WHERE type = 'SQL_COMMAND_RUN_START') t2 on t1.CommandID = t2.commandID;")
@@ -37,19 +37,21 @@ for idx,in_file in enumerate(in_files):
         tables[idx].append(duckdb.execute(f"SELECT CAST(Data->'command' AS VARCHAR) FROM tbl WHERE type='SQL_COMMAND_RUN_START' AND CommandID = {NUM_TABLES+i+(2 if ATTACHED else 0)}").fetchone()[0].split()[1])
 
     for i in range(NUM_QUERIES):
-        times[idx].append(duckdb.execute(f"SELECT ROUND(MEDIAN(end_time-start_time)/1000000000.0,5) FROM times WHERE CommandID >= {QUERIES_START+NUM_ITERATIONS*i} AND CommandID < {QUERIES_START+NUM_ITERATIONS*(1+i)};").fetchone()[0])
+        times[idx] += [l[0] for l in duckdb.execute(f"SELECT ROUND((end_time-start_time)/1000000000.0,5) FROM times WHERE CommandID >= {QUERIES_START+NUM_ITERATIONS*i} AND CommandID < {QUERIES_START+NUM_ITERATIONS*(1+i)};").fetchall()]
         rows_scanned[idx].append(duckdb.execute(f"SELECT ROUND(SUM(CAST(Data->'count' AS INTEGER))/1000000.0,5) AS val FROM tbl WHERE type = 'SCANNED_ROWS' and CommandID = {2*NUM_TABLES+1+i+(2 if ATTACHED else 0)};").fetchone()[0])
 
     duckdb.execute(f"DROP TABLE tbl;")
     duckdb.execute(f"DROP TABLE times;")
 for i in range(NUM_QUERIES):
-    duckdb.execute(f"INSERT INTO Result_time VALUES ({i+1},{",".join([str(time[i]) for time in times])});")
-    duckdb.execute(f"INSERT INTO Result_pruning VALUES ({i+1},{",".join([str(scanned[i]) for scanned in rows_scanned])});")
+    for j in range(NUM_ITERATIONS):
+        idx = i*NUM_ITERATIONS+j
+        duckdb.execute(f"INSERT INTO Result_time VALUES ('Time','s',{i+1},{",".join([str(time[idx]) for time in times])});")
+    duckdb.execute(f"INSERT INTO Result_pruning VALUES ('Rows scanned',millions,{i+1},{",".join([str(scanned[i]) for scanned in rows_scanned])});")
 for i in range(NUM_TABLES):
     if (len(set([tables[j][i] for j in range(NUM_STATS)]))) != 1:
         raise "tables are not in the same order in given input files"
-    duckdb.execute(f"INSERT INTO Result_size VALUES ('{tables[0][i]}',{",".join([str(size[i]) for size in sizes])});")
-    duckdb.execute(f"INSERT INTO Result_ingestion VALUES ('{tables[0][i]}',{",".join([str(time[i]) for time in ingestion_times])});")
+    duckdb.execute(f"INSERT INTO Result_size VALUES ('Size','B','{tables[0][i]}',{",".join([str(size[i]) for size in sizes])});")
+    duckdb.execute(f"INSERT INTO Result_ingestion VALUES ('Ingestion','s','{tables[0][i]}',{",".join([str(time[i]) for time in ingestion_times])});")
 duckdb.execute("COPY Result_time TO 'query_time.csv';")
 duckdb.execute("COPY Result_pruning TO 'pruning.csv';")
 duckdb.execute("COPY Result_size TO 'size.csv';")
--- data_visualiser.py
+++ data_visualiser.py
@@ -7,12 +7,18 @@ data_folder = sys.argv[1]
 for data_file in os.listdir(data_folder):
     if not data_file.endswith('.csv'):
         continue
-    data = np.genfromtxt(f"{data_folder}/{data_file}", delimiter=',',names=True,dtype=None)
+    data = np.atleast_1d(np.genfromtxt(f"{data_folder}/{data_file}", delimiter=',',names=True,dtype=None,deletechars="",replace_space=" "))
+    rows = data.size
+    cols = len(data[0])
+    means = [np.mean([data[j][i] for j in range(rows)]) for i in range(3,cols)]
+    p95 = [np.percentile([data[j][i] for j in range(rows)],95) for i in range(3,cols)]
+    title = data[0][0]
+    unit = data[0][1]
 
-    names = list(data.dtype.names)[1:]
+    names = list(data.dtype.names)[3:]
     penguin_means = {
-        'Mean': (18.35, 18.43, 14.98,5),
-        '95th percentile': (38.79, 48.83, 47.50,5)
+        'Mean': means,
+        '95th percentile': p95
     }
 
     x = np.arange(len(names))  # the label locations
@@ -24,14 +30,14 @@ for data_file in os.listdir(data_folder):
     for attribute, measurement in penguin_means.items():
         offset = width * multiplier
         rects = ax.bar(x + offset, measurement, width, label=attribute)
-        ax.bar_label(rects, padding=3)
+        ax.bar_label(rects, padding=3,fmt="%.3g")
         multiplier += 1
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Length (mm)')
-    ax.set_title('Penguin attributes by species')
+    ax.set_ylabel(f'{title}{f" ({unit})" if unit else ""}')
+    ax.set_title(title)
     ax.set_xticks(x + width, names)
     ax.legend(loc='upper left', ncols=3)
-    ax.set_ylim(0, 250)
+    ax.set_ymargin(0.2)
 
     plt.savefig(f"{data_folder}/{"".join(data_file.split('.')[:-1])}.png")
