29fc9fbe7453bf784ac2863f0dfab34ef5320363
Alexander Pedersen <altp@itu.dk>
2026-03-06 12:21:52 +0100

    cleanup of running scripts

--- run_tpch_correctness.sh
+++
@@ -1,5 +0,0 @@
-dir="correctness_test_tpch"
-mkdir $dir
-./build/release/duckdb_min_max -f sql_queries_tpch_correctness.sql 2> /dev/null > $dir/output_min_max.txt
-./build/release/duckdb_cluster -f sql_queries_tpch_correctness.sql 2> /dev/null > $dir/output_cluster.txt
-./build/release/duckdb_bloom -f sql_queries_tpch_correctness.sql 2> /dev/null > $dir/output_bloom.txt
