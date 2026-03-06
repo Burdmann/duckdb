a1a8eab616a619145106593e2c4e5df6952325d9
Alexander Pedersen <altp@itu.dk>
2026-03-06 06:48:42 +0100

    updated runners to also save duckdb output

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+./run_microbench_unsorted_seconds.sh
+./run_microbench_unsorted_minutes.sh
 ./run_microbench_unsorted_hours.sh
+./run_microbench_sorted_seconds.sh
 ./run_microbench_sorted_minutes.sh
 ./run_microbench_sorted_hours.sh
--- run_microbench_sorted.sh
+++ run_microbench_sorted.sh
@@ -6,13 +6,13 @@ cat template.csv > $dir/raw_data_cluster.csv
 cat template.csv > $dir/raw_data_bloom.csv
 cat template.csv > $dir/raw_data_dictionary.csv
 rm test.db
-./build/release/duckdb_min_max -f sql_queries_sorted.sql 2>> $dir/raw_data_min_max.csv
+./build/release/duckdb_min_max -f sql_queries_sorted.sql 2>> $dir/raw_data_min_max.csv > $dir/output_min_max.txt
 rm test.db
-./build/release/duckdb_cluster -f sql_queries_sorted.sql 2>> $dir/raw_data_cluster.csv
+./build/release/duckdb_cluster -f sql_queries_sorted.sql 2>> $dir/raw_data_cluster.csv > $dir/output_cluster.txt
 rm test.db
-./build/release/duckdb_bloom -f sql_queries_sorted.sql 2>> $dir/raw_data_bloom.csv
+./build/release/duckdb_bloom -f sql_queries_sorted.sql 2>> $dir/raw_data_bloom.csv > $dir/output_bloom.txt
 rm test.db
-./build/release/duckdb_dictionary -f sql_queries_sorted.sql 2>> $dir/raw_data_dictionary.csv
+./build/release/duckdb_dictionary -f sql_queries_sorted.sql 2>> $dir/raw_data_dictionary.csv > $dir/output_dictionary.txt
 rm test.db
 python3 data_processor.py 1 1000 0 $dir/raw_data_min_max.csv $dir/raw_data_cluster.csv $dir/raw_data_bloom.csv $dir/raw_data_dictionary.csv
 mv pruning.csv $dir/processed/
