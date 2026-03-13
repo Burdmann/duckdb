3ff750f6a43ac6cdde48e42a8b5e33dfda805e67
altp <altp@itu.dk>
2026-03-13 23:19:46 +0100

    ran cardinality performance benchmark

---
+++ run_all_cardinality_performance_10.sh
@@ -0,0 +1,4 @@
+./run_performance.sh "xperiments_results/cardinality_100_performance" "sql_queries_cardinality_100_performance.sql"
+./run_performance.sh "xperiments_results/cardinality_1000_performance" "sql_queries_cardinality_1000_performance.sql"
+./run_performance.sh "xperiments_results/cardinality_10000_performance" "sql_queries_cardinality_10000_performance.sql"
+./run_performance.sh "xperiments_results/cardinality_100000_performance" "sql_queries_cardinality_100000_performance.sql"
